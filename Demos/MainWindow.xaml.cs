8f9d81fbf0c506337e581ad1f237b493dd452912
Javier <Javier@JAVIER-LAPTOP>
2018-11-16 19:32:07 +0100

    Agregar archivos de proyecto.

---
+++ Demos/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //var x = lblNombre?.Content ?? "(vacio)";
+
+            InitializeComponent();
+
+            //lblNombre.Content = "Texto";
+            
+        }
+
+        private void Label_ContextMenuClosing(object sender, ContextMenuEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            (new canvas()).Show();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new canvas();
+            w.Owner = this;
+            w.Show();
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            var w = new cdlg();
+            w.Owner = this;
+            w.ShowDialog();
+
+        }
+
+        kk cntr = null;
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(cntr == null)
+                cntr =  new kk();
+            root.Content = cntr;
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            root.Content = new demo2();
+        }
+
+        private void btnAbrirPOP_Click(object sender, RoutedEventArgs e)
+        {
+            popDlg.IsOpen = true;
+        }
+
+        private void btnCerrarPOP_Click(object sender, RoutedEventArgs e)
+        {
+            popDlg.IsOpen = false;
+        }
+    }
+}
---
+++ Demos/kk.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demos
+{
+    /// <summary>
+    /// Lógica de interacción para kk.xaml
+    /// </summary>
+    public partial class kk : UserControl
+    {
+        public kk()
+        {
+            InitializeComponent();
+        }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            lblNombre.Content = "Hola mundo";
+            
+        }
+        private static kk current = null;
+        public static kk Current {
+            get {
+                if (current == null)
+                    current = new kk();
+                return current;
+            }
+        }
+
+        public void reset()
+        {
+            current = null;
+        }
+    }
+}
